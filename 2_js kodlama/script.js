f0621caf06ab8e20a5f2d616aed57c22333f720a
Nurevsansuara <93526756+nurevsangezer@users.noreply.github.com>
2022-09-29 16:06:55 +0300

    Add files via upload

---
+++ 2_js kodlama/script.js
@@ -0,0 +1,30 @@
+let ogr1_ad = "Ada";
+let ogr1_soyad ="Bilgi";
+
+let ogr1_dogumTarihi = "2010";
+
+let ogr1_Matematik1 = 70;
+let ogr1_Matematik2 = 70;
+let ogr1_Matematik3 = 80;
+
+let ogr1_yas = 2022-ogr1_dogumTarihi;
+console.log(ogr1_yas);
+
+let ogr1_ortalama = (ogr1_Matematik1 + ogr1_Matematik2 + ogr1_Matematik3) / 3;
+console.log(parseInt(ogr1_ortalama));
+console.log(ogr1_ortalama >=50) ;
+
+
+
+let ogr2_ad = "Yiğit";
+let ogr2_soyad ="Bilgi";
+
+let ogr2_dogumTarihi = "2012";
+
+let ogr2_Matematik1 = 40;
+let ogr2_Matematik2 = 40;
+let ogr2_Matematik3 = 50;
+let ogr2_ortalama = (ogr2_Matematik1 + ogr2_Matematik2 + ogr2_Matematik3) / 3;
+console.log(parseInt(ogr2_ortalama));
+console.log(ogr2_ortalama >=50) ;
+
